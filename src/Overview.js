f483d2d3bb6d691270615e42005a4ee212ab0847
IMarnild <im.arnild@gmail.com>
2018-05-29 11:55:54 +0200

    modal first stage

--- src/Overview.js
+++ src/Overview.js
@@ -1,32 +1,46 @@
 import React, {Component, Fragment} from 'react'
 import {postJSON} from './util/ajax'
+import Modal from 'react-modal';
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
 
 class Overview extends Component {
-	submitSurvey = formObj => {
-		postJSON('https://syst-api.azurewebsites.net/marktramp/survey', formObj) // Do something with the result - handle errors
-	}
-
 	render() {
 		const { gender, age, food, money, name, artists } = this.props.location.state
 		const { toilet, festival, nature } = this.props.location.state.grades
 
 		return(
-			<Fragment>
-                <div>
-                    <p>Name: {name}</p>
-                    <p>Gender: {gender}</p>
-                    <p>Food: {food}</p>
-                    <p>Money: {money}</p>
-                    <p>Age: {age}</p>
-                    <p>Toilet: {toilet}</p>
-                    <p>Festival: {festival}</p>
-                    <p>Nature: {nature}</p>
-                    {artists.map(artist => (
-                        <p key={artist}> {artist} </p>
-                    ))}
-                </div>
-				<button onClick={() => this.submitSurvey(this.props.location.state)}>Submit</button>
-			</Fragment>
+				<Modal
+                    isOpen={this.props.modalIsOpen}
+                    onAfterOpen={this.props.afterOpenModal}
+                    onRequestClose={this.props.closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+					<h2 ref={subtitle => this.subtitle = subtitle}>Your answers</h2>
+					<p>Name: {name}</p>
+					<p>Gender: {gender}</p>
+					<p>Food: {food}</p>
+					<p>Money: {money}</p>
+					<p>Age: {age}</p>
+					<p>Toilet: {toilet}</p>
+					<p>Festival: {festival}</p>
+					<p>Nature: {nature}</p>
+					{artists.map(artist => (
+                            <p key={artist}> {artist} </p>
+                        ))}
+					<button onClick={this.props.closeModal}>close</button>
+                    <button onClick={() => this.props.submitSurvey(this.props.location.state)}>Submit</button>
+				</Modal>
 		)
 	}	
 }
--- src/Questionnaire.js
+++ src/Questionnaire.js
@@ -1,7 +1,8 @@
 import React, {Fragment} from 'react'
 import {Link} from 'react-router-dom'
-import {getJSON} from './util/ajax'
+import {getJSON, postJSON} from './util/ajax'
 import setInPath from 'lodash/set'
+import Overview from './Overview'
 
 class Questionnaire extends React.Component {
 	state = {
@@ -18,7 +19,8 @@ class Questionnaire extends React.Component {
 			money: 100,
 			food: "",
 		},
-		artists: []
+		artists: [],
+        modalIsOpen: false
 	}
 
 	componentDidMount() {
@@ -51,109 +53,138 @@ class Questionnaire extends React.Component {
         this.handleFormChange('artists', this.state.form.artists.filter(x => x !== artist))
     }
 
+    openModal = () => {
+        this.setState({modalIsOpen: true});
+    }
+
+    afterOpenModal = () => {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal = () => {
+        this.setState({modalIsOpen: false});
+    }
+
+    submitSurvey = formObj => {
+        postJSON('https://syst-api.azurewebsites.net/marktramp/survey', formObj) // Do something with the result - handle errors
+    }
+
+    renderModal = bool => (
+        bool &&
+		<Overview
+			answers={this.state.form}
+            modalIsOpen={this.state.modalIsOpen}
+            afterOpenModal={this.afterOpenModal}
+            closeModal={this.closeModal}
+            submitSurvey={this.submitSurvey}
+        />
+	)
+
 	render() {
-		// This was previously named isArtistDisabled, but we decided to rename it due to PC reasons. :)
-		const isArtistButtonDisabled = artist => {
-			return !this.state.form.artists.includes(artist)
-				&& this.state.form.artists.length === 3
-		}
+	// This was previously named isArtistDisabled, but we decided to rename it due to PC reasons. :)
+	const isArtistButtonDisabled = artist => {
+		return !this.state.form.artists.includes(artist)
+			&& this.state.form.artists.length === 3
+	}
 
-		return(
-			<div className='contact-container'>
-				<form id="contactForm">
-					<label htmlFor="name">Name</label>
-					<input 
-						value={this.state.form.name} 
-						onChange={this.handleInputChange('name')}
-						placeholder="Mark funny guy"
-						required
-					/>
+	return(
+		<div className='contact-container'>
+			<form id="contactForm">
+				<label htmlFor="name">Name</label>
+				<input
+					value={this.state.form.name}
+					onChange={this.handleInputChange('name')}
+					placeholder="Mark funny guy"
+					required
+				/>
 
-					<label htmlFor="gender">Gender</label>
-					<select 
-						value={this.state.form.gender} 
-						onChange={this.handleInputChange('gender')}
-						required
-					>
-						<option value="M"> Mand </option>
-						<option value="F"> Kvinde </option>
-					</select>
+				<label htmlFor="gender">Gender</label>
+				<select
+					value={this.state.form.gender}
+					onChange={this.handleInputChange('gender')}
+					required
+				>
+					<option value="M"> Mand </option>
+					<option value="F"> Kvinde </option>
+				</select>
 
-					<label htmlFor="age">Age</label>
-					<input 
-						value={this.state.form.age} 
-						onChange={this.handleInputChange('age')}
-						type="number"
-						min="15"
-						max="120"
-						required
-					/>
+				<label htmlFor="age">Age</label>
+				<input
+					value={this.state.form.age}
+					onChange={this.handleInputChange('age')}
+					type="number"
+					min="15"
+					max="120"
+					required
+				/>
 
-					<label htmlFor="artists">Artists</label>
-					<div className="artist-menu" id="artists">
-					{this.state.artists.map(artist => (
-						<Fragment key={artist}>
-							<label htmlFor="artist"> {artist} </label>
-							<input 
-								type="checkbox"
-								value={artist}
-								disabled={isArtistButtonDisabled(artist)}
-								onChange={this.handleArtistChange(artist)}
-							/>
-						</Fragment>
-					))}
-					</div>
+				<label htmlFor="artists">Artists</label>
+				<div className="artist-menu" id="artists">
+				{this.state.artists.map(artist => (
+					<Fragment key={artist}>
+						<label htmlFor="artist"> {artist} </label>
+						<input
+							type="checkbox"
+							value={artist}
+							disabled={isArtistButtonDisabled(artist)}
+							onChange={this.handleArtistChange(artist)}
+						/>
+					</Fragment>
+				))}
+				</div>
 
-					<label htmlFor="age">Toilets</label>
-					<input 
-						value={this.state.form.grades.toilet} 
-						onChange={this.handleInputChange('grades.toilet')}
-						type="number"
-						min="1"
-						max="5"
-						required
-					/>
+				<label htmlFor="age">Toilets</label>
+				<input
+					value={this.state.form.grades.toilet}
+					onChange={this.handleInputChange('grades.toilet')}
+					type="number"
+					min="1"
+					max="5"
+					required
+				/>
 
-					<label htmlFor="age">Festival</label>
-					<input 
-						value={this.state.form.grades.festival} 
-						onChange={this.handleInputChange('grades.festival')}
-						type="number"
-						min="1"
-						max="5"
-						required
-					/>
+				<label htmlFor="age">Festival</label>
+				<input
+					value={this.state.form.grades.festival}
+					onChange={this.handleInputChange('grades.festival')}
+					type="number"
+					min="1"
+					max="5"
+					required
+				/>
 
-					<label htmlFor="age">Nature</label>
-					<input 
-						value={this.state.form.grades.nature} 
-						onChange={this.handleInputChange('grades.nature')}
-						type="number"
-						min="1"
-						max="5"
-						required
-					/>
+				<label htmlFor="age">Nature</label>
+				<input
+					value={this.state.form.grades.nature}
+					onChange={this.handleInputChange('grades.nature')}
+					type="number"
+					min="1"
+					max="5"
+					required
+				/>
 
-					<label htmlFor="age">Money</label>
-					<input 
-						value={this.state.form.money} 
-						onChange={this.handleInputChange('money')}
-						type="number"
-						min="1"
-						required
-					/>
+				<label htmlFor="age">Money</label>
+				<input
+					value={this.state.form.money}
+					onChange={this.handleInputChange('money')}
+					type="number"
+					min="1"
+					required
+				/>
 
-					<label htmlFor="food">Food</label>
-					<textarea 
-						value={this.state.form.food} 
-						onChange={this.handleInputChange('food')}
-						rows="20"
-						cols="40" 
-						placeholder="I want to know why there are no pissoires on the school?!?!" required />
-				</form>
+				<label htmlFor="food">Food</label>
+				<textarea
+					value={this.state.form.food}
+					onChange={this.handleInputChange('food')}
+					rows="20"
+					cols="40"
+					placeholder="I want to know why there are no pissoires on the school?!?!" required />
+			</form>
 
-				{/* TODO: Don't allow user to continue, unless data is correct */}
-				<Link to={{pathname: "/overview", state: this.state.form}}> Check Answers </Link>
+			{/* TODO: Don't allow user to continue, unless data is correct */}
+			<button onClick={this.openModal}>Check answers</button>
+			{this.renderModal(this.state.modalIsOpen)}
 			</div>
 		)
 	}
