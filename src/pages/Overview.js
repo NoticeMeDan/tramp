2534d0b7544641696e5fdeb3d815090534fa1b56
Mathias Egekvist <megekvist@me.com>
2018-05-29 21:33:57 +0200

    last styling

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -4,6 +4,16 @@ import {postJSON} from '../util/ajax'
 
 import Modal from 'react-modal'
 
+const modalStyle = {
+	content: {
+		width: '50vw',
+		height: '50vh',
+		top: '25vh',
+		left: '25vw'
+	}	
+}
+
+
 class Overview extends Component {
 	state = {
 		answerModalIsOpen: false
@@ -30,25 +40,27 @@ class Overview extends Component {
 				<img src="assets/celebrate.svg" />
 				<div className="grey-box">
 					<h2>{`Tusinde tak, ${name}!`}</h2>
-					<button onClick={this.toggleModal}>Se dine svar</button>
+					<button onClick={this.toggleModal}>See your answers</button>
 					<Modal
 						isOpen={this.state.answerModalIsOpen}
 						onRequestClose={this.toggleModal}
 						contentLabel='Dine svar'
-						appElement={document.querySelector('#app')}>
-	                    <div>
-	                        <p>Navn: {name}</p>
-	                        <p>Køn: {gender}</p>
-	                        <p>Mad: {food}</p>
-	                        <p>Penge: {money}</p>
-	                        <p>Alder: {age}</p>
+						appElement={document.querySelector('#app')}
+						style={modalStyle}>
+	                    <div className="modal-content">
+	                    	<h2> Your answers </h2>
+	                        <p>Name: {name}</p>
+	                        <p>Gender: {gender}</p>
+	                        <p>Food: {food}</p>
+	                        <p>Money: {money}</p>
+	                        <p>Age: {age}</p>
 	                        <p>Toilet: {toilet}</p>
 	                        <p>Festival: {festival}</p>
-	                        <p>Natur: {nature}</p>
-							<p>Kunstnere: </p>
+	                        <p>Nature: {nature}</p>
+							<p>Artists: </p>
 							<ul>
 	                            {artists.map(artist => (
-	                                <li key={artist}>{artist}</li>
+	                                <li key={artist}>  {artist}</li>
 	                            ))}
 							</ul>
 	                        <button onClick={this.toggleModal}>Luk</button>
